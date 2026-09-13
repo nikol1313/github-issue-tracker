09f68c2effab5688610802c8ab9f1e52794d1baa
Nikusha Avsajanishvili <144850283+nikol1313@users.noreply.github.com>
2026-09-13 14:32:39 +0400

    implemented delete logic, unique id, rendering the tasks and etc

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
 const form = document.querySelector("#task-form");
 const taskArray = [];
+const display = document.querySelector("#display");
 
+let nextId = 1;
 
 form.addEventListener("submit", (foo) => {
     foo.preventDefault();
+
     const newForm = new FormData(form);
-    
-    const name = newForm.get("task-name");
-    const description = newForm.get("desc");
-    const priority = newForm.get("priority");
+
     const load = {
-        name: name,
-        description: description,
-        priority: priority
-    }
-    console.log(load)
-    taskArray.push(load)
+        id: nextId++,
+        name: newForm.get("task-name"),
+        description: newForm.get("desc"),
+        priority: newForm.get("priority")
+    };
+
+    taskArray.push(load);
+
+    renderTasks();
+    form.reset();
 });
 
-console.log(taskArray)
+
+function renderTasks() {
+    display.innerHTML = "";
+
+    taskArray.forEach(task => {
+        display.innerHTML += `
+            <div class="task" data-id="${task.id}">
+                <h3>${task.name}</h3>
+                <p>${task.description}</p>
+                <span>${task.priority}</span>
+                <button type="button" class="deleteB">Delete</button>
+            </div>
+        `;
+    });
+}
+
+
+display.addEventListener("click", (foo) => {
+    // we dont listen to buttonB directly cuz it doesnt exist at first
+    if (foo.target.classList.contains("deleteB")) {
+        const taskElement = foo.target.closest(".task");
+        // get the id from data-id
+        const id = Number(taskElement.dataset.id);
+
+        const index = taskArray.findIndex(task => task.id === id);
+
+        // findIndex return -1, so if not equal -1 we found the element.
+        if (index !== -1) {
+            taskArray.splice(index, 1);
+        }
+
+        renderTasks();
+    }
+});
+
+//search/filter
